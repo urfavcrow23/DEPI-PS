775ac8829f878a1e079112b371fe8748233a508e
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2024-07-12 14:54:53 +0300

    Update repeat-str.js

---
+++ repeat-str.js
@@ -0,0 +1,9 @@
+function repeatString(number, str) {
+  return str.repeat(number);
+  //   let newString = "";
+  //   for (let i = 0; i < number; i++) {
+  //     newString += str;
+  //   }
+  //   return newString;
+}
+console.log(repeatString(9, " hello "));
