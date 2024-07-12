b0aadf11ea4d7f21d3b898fc8a839ac719920283
Karim El Refai <100537642+urfavcrow23@users.noreply.github.com>
2024-07-12 14:56:13 +0300

    Update sum-without-highest-and-lowest-number.js

---
+++ sum-without-highest-and-lowest-number.js
@@ -0,0 +1,20 @@
+function sumWithoutTheHeighestAndLowestNumber(array) {
+  if (array == null) return 0;
+  return array
+    .sort((a, b) => a - b)
+    .slice(1, -1)
+    .reduce((acc, current) => acc + current, 0);
+
+  //   return array
+  //     .filter((x) => x !== Math.max(...array) && x !== Math.min(...array))
+  //     .reduce((acc, current) => acc + current, 0);
+
+  //   let maxValue = Math.max(...array);
+  //   let minValue = Math.min(...array);
+  //   let filteredArray = array.filter((x) => x !== maxValue && x !== minValue);
+  //   let newArray = filteredArray.reduce((acc, current) => acc + current, 0);
+  //   return newArray;
+}
+console.log(
+  sumWithoutTheHeighestAndLowestNumber([1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9])
+);
